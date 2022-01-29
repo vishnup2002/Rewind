2966b6cb03c8dd90f91ee83bd3330e166c02e2a4
Vishnu <vishnupothia@gmail.com>
2022-01-29 16:33:09 +0530

    color

--- lib/screens/signup_Screen.dart
+++ lib/screens/signup_Screen.dart
@@ -37,9 +37,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
-            hexStringToColor("CB2B93"),
-            hexStringToColor("9546C4"),
-            hexStringToColor("5E61F4")
+            hexStringToColor("28313b"),
+            hexStringToColor("485461"),
           ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
           child: SingleChildScrollView(
               child: Padding(
